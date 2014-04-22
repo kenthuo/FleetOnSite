a2e17a010f44d36bfc6fe22b5fc9d6b1d63d3844
Kent Huo <khuo@contigo.com>
2014-04-22 14:59:45 -0700

    add context menu to the map

--- client/js/fleet_on_site.js
+++ client/js/fleet_on_site.js
@@ -3,16 +3,24 @@
  */
 function ContigoMap() {
     this.map = null;
+    this.contextMenu = null;
+    this.current = null; // current click event (used to save as start / end position)
+    this.m1 = null;
+    this.m2 = null;
 }
 
 ContigoMap.prototype = {
 	/**
-	 * Initialize and show the map with the default central position and zoom level.
+	 * Initialize and show the map with the default central position and zoom level, and
+	 * also initialize the context menu.
 	 * 
 	 */
-	init : function() {
+	init: function() {
+		var that = this;
 		this.map = $('#map');
-        $('#map').gmap3({
+		this.contextMenu = new Gmap3Menu(this.map);
+		this.initContextMenu();
+        this.map.gmap3({
             map: {
                 options: {
                     center: [48.16700, -100.16700],
@@ -25,9 +33,48 @@ ContigoMap.prototype = {
                     navigationControl: true,
                     scrollwheel: true,
                     streetViewControl: true
-                }
+                },
+                events:{
+                	rightclick: function(map, event) {
+						that.current = event;
+						that.contextMenu.open(that.current);
+					},
+					click: function() {
+						that.contextMenu.close();
+					},
+					dragstart: function() {
+						that.contextMenu.close();
+					},
+					zoom_changed: function() {
+						that.contextMenu.close();
+					}
+				}
             }
         });
+	},
+	
+	/**
+	 * Add menu item to the context menu.
+	 */
+	initContextMenu: function() {
+		var that = this;
+		this.contextMenu.add("Zoom in", "zoomIn", 
+			function() {
+				var map = that.map.gmap3("get");
+				map.setZoom(map.getZoom() + 1);
+				that.contextMenu.close();
+			});
+		that.contextMenu.add("Zoom out", "zoomOut",
+			function() {
+				var map = that.map.gmap3("get");
+				map.setZoom(map.getZoom() - 1);
+				that.contextMenu.close();
+			});
+		this.contextMenu.add("Center here", "centerHere", 
+			function() {
+				that.map.gmap3("get").setCenter(that.current.latLng);
+				that.contextMenu.close();
+			});
 	}
 }
 
