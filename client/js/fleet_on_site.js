45a2f356fd04a004ec67ce93883f14d7d6af3046
Kent Huo <khuo@contigo.com>
2014-04-22 00:00:06 -0700

    add jquery and gmap3 libraries

--- client/js/fleet_on_site.js
+++ client/js/fleet_on_site.js
@@ -1,3 +1,37 @@
 /*
- * 
+ * JS Library for Fleet On Site.
  */
+function ContigoMap() {
+    this.map = null;
+}
+
+ContigoMap.prototype = {
+	/**
+	 * Initialize and show the map with the default central position and zoom level.
+	 * 
+	 */
+	init : function() {
+		this.map = $('#map');
+        $('#map').gmap3({
+            map: {
+                options: {
+                    center: [48.16700, -100.16700],
+                    zoom: 3,
+                    mapTypeId: google.maps.MapTypeId.ROADMAP,
+                    mapTypeControl: true,
+                    mapTypeControlOptions: {
+                        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+                    },
+                    navigationControl: true,
+                    scrollwheel: true,
+                    streetViewControl: true
+                }
+            }
+        });
+	}
+}
+
+$(document).ready(function() {
+    var map = new ContigoMap();
+    map.init();
+});
